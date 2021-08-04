46b49bcc02ac164d370c554f35bbdd66df1d7406
ManuelLucero <mrlucerov@gmail.com>
2021-08-04 02:15:45 -0400

    Added event listener for mouse-enter & set imgs var for global use

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const imgs = document.getElementsByTagName("img");
+console.log(imgs)
+imgs[1].addEventListener("mouseenter", () =>{
+    imgs[1].style.transform='scale(1.2)';
+    imgs[1].style.transition = 'transform 1s'
+
+})
